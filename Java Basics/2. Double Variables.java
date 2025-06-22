95dc7b7b507a12a8233103cb98be3013c509bb9f
Pratheep Kumar Nadar <49834473+pratheepknadar@users.noreply.github.com>
2025-06-22 19:38:13 +0530

    Create 2. Double Variables.java

---
+++ Java Basics/2. Double Variables.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        double first_number,second_number, answer;
+        first_number = 12.45;
+        second_number = 24.8;
+        answer = first_number + second_number;
+        System.out.println("Addition Total" + answer);
+    }
+}
