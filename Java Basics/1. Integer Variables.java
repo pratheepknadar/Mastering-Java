f12faee8832d79c1d6edef23c61abb8279584471
Pratheep Kumar Nadar <49834473+pratheepknadar@users.noreply.github.com>
2025-06-22 19:33:30 +0530

    int

---
+++ Java Basics/1. Integer Variables.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int first_number,second_number, answer;
+        first_number = 12;
+        second_number = 24;
+        answer = first_number + second_number;
+        System.out.println("Addition Total" + answer);
+    }
+}
