5aa98b856dbe766eae4ce1e34fe2ddbb779772e4
Pratheep Kumar Nadar <49834473+pratheepknadar@users.noreply.github.com>
2025-06-23 22:15:21 +0530

    Basics

---
+++ Java Basics/4. Boolean.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean user = true;
+        if (user == true) {
+            System.out.println("It's Working");
+        }
+
+    }
+}
---
+++ Java Basics/6. FOR Loops.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int startvalue = 10;
+        int endvalue = 20;
+        for (int i = startvalue; i <= endvalue; i++){
+            System.out.println(i);
+
+        }
+
+    }
+}
